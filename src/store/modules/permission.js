50e61d2c83d19079fc7740eaaa6407c761f9c49f
liufashi <liufashi@yuantutech.com>
2022-03-12 13:57:16 +0800

    feat: 雁阵惊寒，声断衡阳之浦

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -1,4 +1,4 @@
-import { asyncRoutes, constantRoutes } from '@/router'
+import { asyncRoutes, constantRoutes } from "@/router";
 
 /**
  * Use meta.role to determine if the current user has permission
@@ -6,10 +6,10 @@ import { asyncRoutes, constantRoutes } from '@/router'
  * @param route
  */
 function hasPermission(roles, route) {
-  if (route.meta && route.meta.roles) {
-    return roles.some(role => route.meta.roles.includes(role))
+  if (route.meta?.roles) {
+    return roles.some((role) => route.meta.roles.includes(role));
   } else {
-    return true
+    return true;
   }
 }
 
@@ -19,51 +19,46 @@ function hasPermission(roles, route) {
  * @param roles
  */
 export function filterAsyncRoutes(routes, roles) {
-  const res = []
+  const res = [];
 
-  routes.forEach(route => {
-    const tmp = { ...route }
+  routes.forEach((route) => {
+    const tmp = { ...route };
     if (hasPermission(roles, tmp)) {
       if (tmp.children) {
-        tmp.children = filterAsyncRoutes(tmp.children, roles)
+        tmp.children = filterAsyncRoutes(tmp.children, roles);
       }
-      res.push(tmp)
+      res.push(tmp);
     }
-  })
+  });
 
-  return res
+  return res;
 }
 
 const state = {
   routes: [],
-  addRoutes: []
-}
+  addRoutes: [],
+};
 
 const mutations = {
   SET_ROUTES: (state, routes) => {
-    state.addRoutes = routes
-    state.routes = constantRoutes.concat(routes)
-  }
-}
+    state.addRoutes = routes;
+    state.routes = constantRoutes.concat(routes);
+  },
+};
 
 const actions = {
   generateRoutes({ commit }, roles) {
-    return new Promise(resolve => {
-      let accessedRoutes
-      if (roles.includes('admin')) {
-        accessedRoutes = asyncRoutes || []
-      } else {
-        accessedRoutes = filterAsyncRoutes(asyncRoutes, roles)
-      }
-      commit('SET_ROUTES', accessedRoutes)
-      resolve(accessedRoutes)
-    })
-  }
-}
+    return new Promise((resolve) => {
+      const accessedRoutes = filterAsyncRoutes(asyncRoutes, roles);
+      commit("SET_ROUTES", accessedRoutes);
+      resolve(accessedRoutes);
+    });
+  },
+};
 
 export default {
   namespaced: true,
   state,
   mutations,
-  actions
-}
+  actions,
+};
