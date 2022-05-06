2c275703ec9892c381a801f0710f1efd51644d51
liufashi <liufashi@yuantutech.com>
2022-05-06 11:25:58 +0800

    home

--- src/views/home/editor/index.vue
+++ src/views/home/editor/index.vue
@@ -1,152 +1,154 @@
 <template>
-  <div style="margin: 10px" >
-    <!--与上边距相差 20px的距离-->
-    <el-row class="home" :gutter="20">
-      <!--左边占据 8 -->
-      <el-col :lg='{span:6}'>
-        <!--上面个人信息-->
-        <el-card shadow="hover" class="box-card" style="min-height:210px">
-          <el-col :lg='{span:12}'>
-            <img class="avatar" :src="avatar" alt="" />
-          </el-col>
-          <el-col :lg='{span:12}' style="font-size:13px">
-            <p >用户名：{{ name }}</p>　
-            <p>用户角色：{{ roleName }}</p>　
-            <p>兴趣：code & game & food</p>
-            <p>个性签名：暂无暂无暂无暂无暂无暂无</p>
-          </el-col>
-        </el-card>
-        <!--下面信息框-->
-        <el-card shadow="hover" style="min-height: 633px; margin-top: 20px">
-         <TransactionTable v-if='messageCount&&messageCount.length' :list="messageCount"/>
-        </el-card>
+  <div style="margin: 10px" class="home">
+    <el-card shadow="hover" class="box-card" style="min-height: 210px">
+      <el-col :lg="{ span: 4 }">
+        <img class="avatar" :src="avatar" alt="" />
       </el-col>
-      <!--右边占据16-->
-
-    </el-row>
+      <el-col :lg="{ span: 8 }" style="font-size: 13px">
+        <p>欢迎登录，以下是您的账号信息</p>
+        <p>用户名：{{ name }}</p>
+        　
+        <p>用户角色：{{ roleName }}</p>
+        　
+        <p v-if="roleName === '学生'">
+          系统功能：申请奖项、修改个人信息、查看申请信息
+        </p>
+        <p v-else-if="roleName === '教务处'">
+          系统功能：奖项查阅、查看步骤、管理奖项审核流程、导出评选结果
+        </p>
+        <p v-else>系统功能：奖项审核、查看步骤</p>
+      </el-col>
+    </el-card>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import getRoleName from '@/utils/getRoleName'
-import { getToken } from '@/utils/auth'
-import LineChart from '../admin/components/LineChart.vue'
-import PieChart from '../admin/components/PieChart.vue'
-import TransactionTable from '../admin/components/TransactionTable.vue'
-import {
-  getRewards,
-  getApplyList,
-  getRewardResult
-} from "@/api/rewards";
-import user from '@/store/modules/user'
-import {
-  getStudent,
-} from "@/api/manage";
-import CountTo from 'vue-count-to'
+import { mapGetters } from "vuex";
+import getRoleName from "@/utils/getRoleName";
+import { getToken } from "@/utils/auth";
+import LineChart from "../admin/components/LineChart.vue";
+import PieChart from "../admin/components/PieChart.vue";
+import TransactionTable from "../admin/components/TransactionTable.vue";
+import { getRewards, getApplyList, getRewardResult } from "@/api/rewards";
+import user from "@/store/modules/user";
+import { getStudent } from "@/api/manage";
+import CountTo from "vue-count-to";
 export default {
-  data () {
+  data() {
     return {
-      roleName: '',
-      messageCount:null,
+      roleName: "",
+      messageCount: null,
       nums: [],
       chartData: null,
-      rewardsData:null
-    }
+      rewardsData: null,
+    };
   },
   computed: {
-    ...mapGetters(['name', 'avatar', 'roles']),
+    ...mapGetters(["name", "avatar", "roles"]),
   },
   components: {
     LineChart,
     PieChart,
     TransactionTable,
-    CountTo
+    CountTo,
   },
-  async mounted () {
-    this.roleName = getRoleName(getToken())
-    await this.getStudentList()
-    await this.getRewardsList()
-    await this.getMessageCount()
-    await this.promiseRewardId()
+  async mounted() {
+    this.roleName = getRoleName(getToken());
+    await this.getStudentList();
+    await this.getRewardsList();
+    await this.getMessageCount();
+    await this.promiseRewardId();
   },
   methods: {
-    async getStudentList () {
+    async getStudentList() {
       let data = {
         collegeId: "",
         grade: "",
         majorId: "",
         studentName: "",
         studentNumber: "",
-        typeId: ""
-      }
+        typeId: "",
+      };
       const { total } = await getStudent(data);
       const total_user = {
-        name: '用户个数',
+        name: "用户个数",
         value: total,
-        icon: require('@/assets/home/user.png'),
-        link: 'userManage/index'
-      }
-      this.nums.push(total_user)
+        icon: require("@/assets/home/user.png"),
+        link: "userManage/index",
+      };
+      this.nums.push(total_user);
     },
-    async getRewardsList () {
+    async getRewardsList() {
       const { data, total } = await getRewards();
       const total_reward = {
-        name: '奖项个数',
+        name: "奖项个数",
         value: total,
-        icon: require('@/assets/home/graph-bar.png'),
-        link: 'rewardsDeclare/rewardList'
-      }
-      this.rewardsData = data
-      this.nums.push(total_reward)
+        icon: require("@/assets/home/graph-bar.png"),
+        link: "rewardsDeclare/rewardList",
+      };
+      this.rewardsData = data;
+      this.nums.push(total_reward);
     },
-    async getMessageCount () {
+    async getMessageCount() {
       const { data } = await getApplyList({ role: getToken() });
       const total_apply = {
-        name: '奖项申请人数',
+        name: "奖项申请人数",
         value: data.length,
-        icon: require('@/assets/home/envelope.png'),
-        link: 'rewardsDeclare/applyList'
-      }
-      this.messageCount = data
-      const total_apply_ing_length = data.filter(item => item.applyStatus == 1)
-      const total_apply_up_length = data.filter(item => item.applyStatus == 2)
-      const total_apply_ret_length = data.filter(item => item.applyStatus == 0)
-      const total_apply_out_length = data.filter(item => item.applyStatus == -2)
+        icon: require("@/assets/home/envelope.png"),
+        link: "rewardsDeclare/applyList",
+      };
+      this.messageCount = data;
+      const total_apply_ing_length = data.filter(
+        (item) => item.applyStatus == 1
+      );
+      const total_apply_up_length = data.filter(
+        (item) => item.applyStatus == 2
+      );
+      const total_apply_ret_length = data.filter(
+        (item) => item.applyStatus == 0
+      );
+      const total_apply_out_length = data.filter(
+        (item) => item.applyStatus == -2
+      );
       const total_apply_ing = {
-        name: '奖项审核中人数',
+        name: "奖项审核中人数",
         value: total_apply_ing_length.length,
-        icon: require('@/assets/home/clock.png'),
-        link: 'allCheck/index'
-      }
+        icon: require("@/assets/home/clock.png"),
+        link: "allCheck/index",
+      };
       const total_apply_up = {
-        name: '奖项通过人数',
+        name: "奖项通过人数",
         value: total_apply_up_length.length,
-        icon: require('@/assets/home/check.png'),
-        link: 'allCheck/index'
-      }
+        icon: require("@/assets/home/check.png"),
+        link: "allCheck/index",
+      };
       const total_apply_out = {
-        name: '奖项未通过人数',
+        name: "奖项未通过人数",
         value: total_apply_out_length.length,
-        icon: require('@/assets/home/close.png'),
-        link: 'allCheck/index'
-      }
+        icon: require("@/assets/home/close.png"),
+        link: "allCheck/index",
+      };
       const total_apply_ret = {
-        name: '奖项驳回人数',
+        name: "奖项驳回人数",
         value: total_apply_ret_length.length,
-        link: 'allCheck/index'
-      }
-      this.nums.push(total_apply, total_apply_ing, total_apply_up, total_apply_out)
+        link: "allCheck/index",
+      };
+      this.nums.push(
+        total_apply,
+        total_apply_ing,
+        total_apply_up,
+        total_apply_out
+      );
       // 模拟数据
-      let index = new Date().getDay()
-      let expectedData = [3, 5, 6, 2, 1, 2, 0]
-      expectedData[index - 1] = total_apply_up.value
-      let actualData = [2, 3, 1, 2, 5, 5, 0]
-      actualData[index - 1] = total_apply_out.value
+      let index = new Date().getDay();
+      let expectedData = [3, 5, 6, 2, 1, 2, 0];
+      expectedData[index - 1] = total_apply_up.value;
+      let actualData = [2, 3, 1, 2, 5, 5, 0];
+      actualData[index - 1] = total_apply_out.value;
       this.chartData = {
         expectedData,
-        actualData
-      }
+        actualData,
+      };
       // this.$nextTick(()=>{
       //   this.$refs.PieChart_pie.chartData = [
       //   total_apply_up,
@@ -156,31 +158,34 @@ export default {
       // ]
       // })
     },
-    async promiseRewardId(){
-       const rewardId = this.rewardsData.map(item => {
+    async promiseRewardId() {
+      const rewardId = this.rewardsData.map((item) => {
         return {
           name: item.rewardName,
-          id: item.id
-        }
-      })
+          id: item.id,
+        };
+      });
       const PromiseAll = rewardId.map(
-        item => new Promise(resolve => {
-          getRewardResult({ rewardId: item.id }).then(res => {
-            resolve(res)
-          }).catch(req => {
-            // 处理promise all 异常
-            return Promise.resolve(req)
+        (item) =>
+          new Promise((resolve) => {
+            getRewardResult({ rewardId: item.id })
+              .then((res) => {
+                resolve(res);
+              })
+              .catch((req) => {
+                // 处理promise all 异常
+                return Promise.resolve(req);
+              });
           })
-        })
-      )
-      let res = await Promise.all(PromiseAll)
-      res.filter((item, index) => rewardId[index].value = item.data.length)
+      );
+      let res = await Promise.all(PromiseAll);
+      res.filter((item, index) => (rewardId[index].value = item.data.length));
       // this.$nextTick(()=>{
       //   this.$refs.PieChart_raddar.chartData = rewardId
       // })
-    }
+    },
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -241,4 +246,3 @@ export default {
   }
 }
 </style>
-
